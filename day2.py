addecc88d1dd5c792b55660243b4538bde352fc1
munalkhk <munalkdk@gmail.com>
2025-08-03 15:57:17 +0800

    day 2

--- day2.py
+++ day2.py
@@ -1,3 +1,7 @@
-
-
-print("hello " + input("what is your name ?") + "!") # wow i m loving python cooding
+name = "Munal"
+print(name)
+name = "Hm"
+print(name)
+name = (input("what is your name?"))
+print(name)
+print(len(input("what is your name ?")))
