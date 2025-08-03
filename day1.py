5c7c143384add6d282cab2025ce2843907ee352a
munalkhk <munalkdk@gmail.com>
2025-08-03 15:38:57 +0800

    added day1 practice

--- day1.py
+++ day1.py
@@ -1,1 +1,2 @@
 print("Test")
+print("hello " + input("what is your name ?") + "!") # wow i m loving python cooding
--- day2.py
+++ day2.py
@@ -1,3 +1,3 @@
-# this is day tow code
+
 
 print("hello " + input("what is your name ?") + "!") # wow i m loving python cooding
