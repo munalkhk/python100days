414ae0010d4c9f82cb88d235f34523fa7759758d
munalkhk <munalkdk@gmail.com>
2025-08-03 16:42:34 +0800

    day2 practice

--- day2.py
+++ day2.py
@@ -5,3 +5,8 @@ print(name)
 name = (input("what is your name?"))
 print(name)
 print(len(input("what is your name ?")))
+#extra 
+username = input("what is your name ?")
+length =len(username)
+print(username)
+print(length)
